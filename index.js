9e3dc45a4878174490850fee835bfff46b7104e7
Aron <aron.kipkorir@student.moringaschool.com>
2024-06-26 22:15:42 +0300

    arithmetic-lab

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+const random = Math.floor(Math.random() * 100) + 1; 
+
+const num3 = 16;
+const num4 = 6;
+const mod = num3 % num4;
+
+ let numbers = [5, 10, 20, 6];
+
+ let max = numbers.reduce( (acc, val) => {
+    return acc > val ? acc : val;
+ });
+
+ console.log("numbers", max);
